dd757691fa4df940f3ae0614bcef54c102ce4c10
Nefil <48553218+xXNefilXx@users.noreply.github.com>
2022-05-25 21:10:16 +0545

    Update main.py

--- main.py
+++ main.py
@@ -37,33 +37,40 @@ class Block():
 
 
 class Blockchain():
-    difficulty=5
+    difficulty=4
 
 
     def __init__(self,chain=[]):
         self.chain=chain
 
 
+
     def add(self,block):
-        self.chain.append({
-            'hash': block.hash(),
-            'previous': block.previous_hash,
-            'number': block.number,
-            'data':block.data,
-            'nonce': block.nonce
-        })
+        self.chain.append(block)
+
+    def remove(self,block):
+        self.chain.remove(block)
 
 
     def mine(self,block):
         try:
-            block.previous_hash=self.chain[-1].get('hash')
+            block.previous_hash=self.chain[-1].hash()
         except IndexError:
             pass
         while True:
-            if block.hash()[:self.difficulty] == 'd'* self.difficulty:
+            if block.hash()[:self.difficulty] == '0'* self.difficulty:
                 self.add(block); break
             else:
                 block.nonce+=1
+
+    def isValid(self):
+        for i in range(1,len(self.chain)):
+            _previous = self.chain[i].previous_hash
+            _current = self.chain[i-1].hash()
+            if _previous!=_current or _current[:self.difficulty] != '0' * self.difficulty:
+                return False
+        return True
+
 
 
 
@@ -92,6 +99,10 @@ def main():
 
     for block in blockchain.chain:
         print(block)
+
+    blockchain.chain[2].data = "new"
+    blockchain.mine(blockchain.chain[2])
+    print(blockchain.isValid())
 
 
 
